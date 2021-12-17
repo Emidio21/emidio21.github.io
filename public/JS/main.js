be62f45cb9673076a42238b3e04a8e1cab43c8b3
Emidio <pierroemy97@gmail.com>
2021-12-17 13:19:44 +0100

    IstruzioneOpenArrow

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,18 +1,28 @@
-// $('#middleSec').html(
-//   `
-//   <h3>HELLO</h3>
-//   <hr />
-//   <p>
-//   Hey
-//   </p>
-//   `
-// );
-
-
 $('#NavBar a').on('click', function (e) {
   e.preventDefault();
   $('a.active').removeClass('active');
   $(this).addClass('active');
   $('#middleSec .div').hide();
   $($(this).attr('href')).show();
+})
+
+
+var unisaCollapse = document.getElementById('Unisa')
+unisaCollapse.addEventListener('show.bs.collapse', function () {
+  $('#UnisaArrow').addClass("fa-chevron-up");
+  $('#UnisaArrow').removeClass("fa-chevron-down")
+})
+unisaCollapse.addEventListener('hide.bs.collapse', function () {
+  $('#UnisaArrow').addClass("fa-chevron-down");
+  $('#UnisaArrow').removeClass("fa-chevron-up")
+})
+
+var superioriCollapse = document.getElementById('Superiori')
+superioriCollapse.addEventListener('show.bs.collapse', function () {
+  $('#SuperioriArrow').addClass("fa-chevron-up");
+  $('#SuperioriArrow').removeClass("fa-chevron-down")
+})
+superioriCollapse.addEventListener('hide.bs.collapse', function () {
+  $('#SuperioriArrow').addClass("fa-chevron-down");
+  $('#SuperioriArrow').removeClass("fa-chevron-up")
 })
